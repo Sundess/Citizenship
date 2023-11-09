05a9410a9d71eeb0ebce981f23907fccbc47c391
Sandesh <sundess17@gmail.com>
2023-11-09 19:09:11 +0545

    test

--- src/main/java/com/example/testproject/HelloApplication.java
+++ src/main/java/com/example/testproject/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("question.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
